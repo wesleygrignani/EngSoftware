02d9bdf6d37758414a6d6b9563e0343b849a59ed
Wesley <wesley.grignani@edu.univali.br>
2019-08-24 15:32:15 -0300

    Menu feito

---
+++ app/src/main/java/com/example/software/CorpoHumano.java
@@ -0,0 +1,17 @@
+package com.example.software;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CorpoHumano extends AppCompatActivity {
+
+    Button jogo1, jogo2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_corpohumano);
+    }
+}
--- app/src/main/java/com/example/software/MainActivity.java
+++ app/src/main/java/com/example/software/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.software;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button jogo1,jogo2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jogo1 = findViewById(R.id.jogo1);
+        jogo2 = findViewById(R.id.jogo2);
     }
 
-    public void teste(){}
-}
+
+    public void primeiroJogo(View v){
+        startActivity(new Intent(getBaseContext(),CorpoHumano.class));
+    }
+
+    public void segundoJogo(View v){
+        startActivity(new Intent(getBaseContext(),CorpoHumano.class));
+
+    }
+
+
+
+    }
+
